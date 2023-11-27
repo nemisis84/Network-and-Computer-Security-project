86a2ba5974050f5ef8cf4136189987eb94983ba0
nemisis84 <simen.myrrusten@gmail.com>
2023-11-27 13:32:14 +0000

    Implemented Joao's changes

---
+++ src/secure_document/API.java
@@ -0,0 +1,169 @@
+package src.secure_document;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+
+public class API {
+    public static void protect(String in_file, String out_file) throws Exception {
+        
+        try (FileReader fileReader = new FileReader(in_file)) {
+            Gson gson = new Gson();
+            JsonObject rootJson = gson.fromJson(fileReader, JsonObject.class);
+
+            String hash_org = Crypto_LIB.Hmac(rootJson.toString(), "bob1234");
+            FileWriter fileWriter = new FileWriter("resources/message.txt");
+            PrintWriter printWriter = new PrintWriter(fileWriter);
+            printWriter.print(hash_org + "\n");
+
+            JsonObject media = rootJson.get("media").getAsJsonObject();
+            JsonObject mediaContent = media.get("mediaContent").getAsJsonObject();
+
+            String audioBase64 = mediaContent.get("audioBase64").getAsString();
+            
+            String cipherB64dString = Crypto_LIB.AES_encrypt(audioBase64, "resources/secret.key");
+
+            mediaContent.addProperty("audioBase64", cipherB64dString);
+            media.add("mediaContent", mediaContent);
+            rootJson.add("media", media);
+
+            // Write JSON object to file
+            try (FileWriter fileWriter2 = new FileWriter(out_file)) {
+                gson = new GsonBuilder().setPrettyPrinting().create();
+                gson.toJson(rootJson, fileWriter2);
+            }
+
+
+            String hash_sec = Crypto_LIB.Hmac(rootJson.toString(), "bob1234");
+            String nonce = "4J8pirLzX6oIF0IIIaUU";  
+            String timeStamp = new SimpleDateFormat("yyyyMMddHHmmss").format(new Date());  
+            printWriter.print(hash_sec + "\n");
+            printWriter.print(nonce + "\n");  // nonce
+            printWriter.print(timeStamp);  // timestamp
+            printWriter.close();
+            
+        }
+    }
+
+    public static void unprotect(String in_file, String out_file) throws Exception {
+        
+        
+        try (FileReader fileReader = new FileReader(in_file)) {
+            Gson gson = new Gson();
+            JsonObject rootJson = gson.fromJson(fileReader, JsonObject.class);
+            JsonObject media = rootJson.get("media").getAsJsonObject();
+            JsonObject mediaContent = media.get("mediaContent").getAsJsonObject();
+
+            String cipherB64dString = mediaContent.get("audioBase64").getAsString();
+            
+            String original = Crypto_LIB.AES_decrypt(cipherB64dString, "resources/secret.key");
+
+            mediaContent.addProperty("audioBase64", original);
+            media.add("mediaContent", mediaContent);
+            rootJson.add("media", media);
+
+            // Write JSON object to file
+            try (FileWriter fileWriter = new FileWriter(out_file)) {
+                gson = new GsonBuilder().setPrettyPrinting().create();
+                gson.toJson(rootJson, fileWriter);
+            } 
+            
+        }
+        
+        
+    }
+
+    public static int check(String in_file) throws Exception {
+        
+        // Read JSON object from file, and print its contets
+        try (FileReader fileReader = new FileReader(in_file)) {
+            Gson gson = new Gson();
+            JsonObject rootJson = gson.fromJson(fileReader, JsonObject.class);
+            
+            //System.out.println("JSON object: " + rootJson + "\n");
+
+            BufferedReader br= new BufferedReader(new FileReader("Files/message.txt"));
+            String hash_org = br.readLine();
+            String hash_sec = br.readLine();
+            br.close();
+            String hash_read = Crypto_LIB.Hmac(rootJson.toString(), "bob1234");
+
+            if(hash_org.equals(hash_read)){//unprotected
+                return 0;
+            }
+            else if(hash_sec.equals(hash_read)){//protected
+                return 1;
+            }
+            else{
+                return -1;
+            }
+        }
+    }
+
+    
+
+
+    public static void main(String[] args) throws Exception{
+        
+        if (args.length < 1) {
+            System.err.println("Argument(s) missing!");
+            return;
+        }
+
+        if(args[0].equals("protect") || args[0].equals("Protect")){
+            if (args.length < 0) {
+                System.err.println("Argument(s) missing!");
+                return;
+            }
+
+            System.out.println("Protect()");
+            protect("resources/secure_doc.json", "resources/encrypted_file");
+        }
+        
+        else if(args[0].equals("unprotect") || args[0].equals("Unprotect")){
+            if (args.length < 0) {
+                System.err.println("Argument(s) missing!");
+                return;
+            }
+            System.out.println("Unprotect()");
+            int flag = check("Files/secure_doc.json");
+            if(flag != 1){
+                if(flag == -1){
+                    System.out.println("The document was tempered with");
+                    return;
+                }
+                if(flag == 0){
+                    System.out.println("The document is already unprotected");
+                    return;
+                }
+            }
+            unprotect("Files/secure_doc.json", "Files/secure_doc.json");
+        }
+
+        else if(args[0].equals("check") || args[0].equals("Check")){
+            int flag = check("Files/secure_doc.json");
+            if(flag == 1){
+                System.out.println("The document is protected");
+                return;
+            }
+
+            if(flag == -1){
+                System.out.println("The document was tempered with");
+                return;
+            }
+
+            if(flag == 0){
+                System.out.println("The document is unprotected");
+                return;
+            }
+            
+        }
+    }
+}
---
+++ src/secure_document/Crypto_LIB.java
@@ -0,0 +1,97 @@
+package src.secure_document;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.security.InvalidKeyException;
+import java.security.Key;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.util.Arrays;
+import java.util.Base64;
+
+import javax.crypto.Cipher;
+import javax.crypto.Mac;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+
+public class Crypto_LIB {
+
+    public static String Hmac(String target, String key) throws NoSuchAlgorithmException, IllegalStateException, UnsupportedEncodingException, InvalidKeyException{
+        Mac mac = Mac.getInstance("HmacSHA256");
+            SecretKeySpec HmacKey = new SecretKeySpec(key.getBytes(), "HmacSHA256");
+            mac.init(HmacKey);
+            return byteArrayToHexString(mac.doFinal(target.getBytes("utf-8")));
+    }
+
+    public static String AES_encrypt(String target, String keyPath) throws Exception{
+
+        byte[] audioBytes = target.getBytes();
+
+        Key secretKey = readSecretKey(keyPath);
+
+        Cipher cipher = Cipher.getInstance("AES/CTR/NoPadding");
+
+        byte[] iv = new byte[16];
+        new SecureRandom().nextBytes(iv);
+        IvParameterSpec ivSpec = new IvParameterSpec(iv);
+
+        cipher.init(Cipher.ENCRYPT_MODE, secretKey, ivSpec);
+
+        byte[] encryptedBytes = cipher.doFinal(audioBytes);
+        byte[] encryptedIVAndText = new byte[iv.length + encryptedBytes.length];
+        System.arraycopy(iv, 0, encryptedIVAndText, 0, iv.length);
+        System.arraycopy(encryptedBytes, 0, encryptedIVAndText, iv.length, encryptedBytes.length);
+
+        String cipherString = Base64.getEncoder().encodeToString(encryptedIVAndText);
+
+        return cipherString;
+        
+    }
+
+    public static String AES_decrypt(String target, String keyPath) throws Exception{
+
+        Key secretKey = readSecretKey(keyPath);
+
+        byte[] encryptedIVAndText = Base64.getDecoder().decode(target);
+
+        byte[] iv = Arrays.copyOfRange(encryptedIVAndText, 0, 16);
+        byte[] cipherBytes = Arrays.copyOfRange(encryptedIVAndText, 16, encryptedIVAndText.length);
+        
+        IvParameterSpec ivSpec = new IvParameterSpec(iv);
+        System.out.println("IV: " + ivSpec.getIV());
+        Cipher cipher = Cipher.getInstance("AES/CTR/NoPadding");
+        System.out.println("Deciphering ...");
+        cipher.init(Cipher.DECRYPT_MODE, secretKey, ivSpec);
+
+        byte[] originalBytes = cipher.doFinal(cipherBytes);
+
+        String original = new String(originalBytes);
+
+        return original;
+        
+    }
+
+    private static String byteArrayToHexString(byte[] array) {
+        StringBuilder hexString = new StringBuilder();
+        for (byte b : array) {
+            hexString.append(String.format("%02x", b));
+        }
+        return hexString.toString();
+    }
+
+    private static byte[] readFile(String path) throws FileNotFoundException, IOException {
+        FileInputStream fis = new FileInputStream(path);
+        byte[] content = new byte[fis.available()];
+        fis.read(content);
+        fis.close();
+        return content;
+    }
+
+    public static Key readSecretKey(String secretKeyPath) throws Exception {
+        byte[] encoded = readFile(secretKeyPath);
+        SecretKeySpec keySpec = new SecretKeySpec(encoded, "AES");
+        return keySpec;
+    }
+}
