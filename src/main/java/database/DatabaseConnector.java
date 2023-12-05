68daa115c62420fd814c65edf2c1c2236f650f31
DanielGomes0 <115481280+DanielGomes0@users.noreply.github.com>
2023-12-05 10:36:10 +0000

    Update DatabaseConnector.java
    Made it so it works for different inputs
    still needs some testing
    
    this should work:
    java database.DatabaseConnector eduardo_albino WAV Ed_Sheeran Shape_of_you pop

--- src/main/java/database/DatabaseConnector.java
+++ src/main/java/database/DatabaseConnector.java
@@ -1,47 +1,57 @@
 package database;
+
 import java.io.*;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-
-import secure_document.API;
-
 import java.nio.charset.StandardCharsets;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
+import secure_document.API;
+
 public class DatabaseConnector {
     private static final String DB_URL = "jdbc:postgresql://localhost:5432/SongsDatabase";
-    // admin for now
     private static final String USER = "admin";
     private static final String PASSWORD = "admin123";
 
     public static void main(String[] args) {
+        if (args.length < 5) {
+            System.err.println("Usage: DatabaseConnector <owner> <format> <artist> <title> <genre1>");
+            System.exit(1);
+        }
+
+        String owner = args[0];
+        String format = args[1];
+        String artist = args[2];
+        String title = args[3];
+        String genre1 = args[4];
+
         try {
-            // loads the PostgreSQL JDBC driver
+            // Load the PostgreSQL JDBC driver
             Class.forName("org.postgresql.Driver");
 
-            // establishes the database connection
+            // Establish the database connection
             try (Connection connection = DriverManager.getConnection(DB_URL, USER, PASSWORD)) {
-                // uses the protect function from the API class
+                // Use the protect function from the API class
                 API.protect("resources/secure_doc.json", "resources/encrypted_file");
 
-                // put the data into the database
+                // Insert the data into the database
                 try (PreparedStatement preparedStatement = connection.prepareStatement(
                         "INSERT INTO media (owner, format, artist, title, genre1) VALUES (?, ?, ?, ?, ?)")) {
-                    preparedStatement.setString(1, "eduardo_albino");
-                    preparedStatement.setString(2, "WAV");
-                    preparedStatement.setString(3, "Ed_Sheeran");
-                    preparedStatement.setString(4, "Shape_of_you");
-                    preparedStatement.setString(5, "pop");
+                    preparedStatement.setString(1, owner);
+                    preparedStatement.setString(2, format);
+                    preparedStatement.setString(3, artist);
+                    preparedStatement.setString(4, title);
+                    preparedStatement.setString(5, genre1);
 
-                    // will have to add more rows once we use everything (lyrics, genre2 etc)
+                    // You can add more parameters and set them accordingly
 
                     preparedStatement.executeUpdate();
                 }
 
-                // read the protected data from the file
+                // Read the protected data from the file
                 StringBuilder protectedData = new StringBuilder();
                 try (BufferedReader reader = new BufferedReader(new FileReader("resources/encrypted_file"))) {
                     String line;
@@ -50,7 +60,7 @@ public class DatabaseConnector {
                     }
                 }
 
-                // sends the protected data to the application server
+                // Send the protected data to the application server
                 sendToApplicationServer(protectedData.toString());
 
             } catch (SQLException e) {
