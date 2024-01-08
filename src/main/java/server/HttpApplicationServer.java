2d185a190326a37d4c8a773733f17b803db22922
nemisis84 <simen.myrrusten@gmail.com>
2024-01-08 10:53:10 +0100

    Removed missing ; error

--- src/main/java/server/HttpApplicationServer.java
+++ src/main/java/server/HttpApplicationServer.java
@@ -203,7 +203,6 @@ public class HttpApplicationServer {
             String sessionkeypath = "Clients/" + client + "/session.key";
             if(type.equals("NORMAL")){
                 enckeypath = "Clients/" + client + "/secret.key";
-                System.out.println()
             }
             else{
                 f = new File("Clients/" + client + "/family.txt");
